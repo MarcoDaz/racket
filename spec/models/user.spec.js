68edc4492e3def54c5a79b8b4a353cf2dca71514
Adrian Fee <adrian.fee@yapily.com>
2022-10-05 14:43:44 +0100

    jest tests for each user attribute passing, need to work on basket

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -9,7 +9,6 @@ describe("User model", () => {
       done();
     });
   });
-
   it("has an email address", () => {
     const user = new User({
       username: "username",
@@ -18,5 +17,23 @@ describe("User model", () => {
       basket: []
     });
     expect(user.email).toEqual("someone@example.com");
+  });
+  it("has a password", () => {
+    const user = new User({
+      username: "username",
+      email: "someone@example.com",
+      password: "password",
+      basket: []
+    });
+    expect(user.password).toEqual("password");
+  });
+  it("has a basket", () => {
+    const user = new User({
+      username: "username",
+      email: "someone@example.com",
+      password: "password",
+      basket: []
+    });
+    expect(user.basket).toEqual([]);
   });
 });
