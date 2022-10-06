7ac322166e29c8132bd369de4db9670dfe4f81e0
Adrian Fee <adrian.fee@yapily.com>
2022-10-06 14:34:27 +0100

    testing out graphs in the ejs file

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,17 @@
+const Chart = require('../models/chart.js');
+const products = require('../trolley/products.js');
+const Product = require('../models/product.js');
+const createChart = require('../views/home/script.js')
+
 const HomeController = {
-  Index: (req, res) => {
+  Index: async (req, res) => {
     console.log("############## Now in HomeController Index ##############")
-    res.render("home/index", {});
+
+    const products = await Product.find().exec();
+    res.render("home/index", {
+      products: products,
+      createChart: createChart,
+    });
   }
 }
 
