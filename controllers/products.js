9d6524063c91c7c96d19afd1be2b96c1ab391022
chimaamu <chimaamu@proton.me>
2022-10-06 12:12:07 +0100

    changes made to html page for adding products

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,17 @@ const Product = require("../models/product");
 
 const ProductsController = {
   Index: (req, res) => {
-    console.log("############## Now in ProductController New ##############")
+    console.log("############## Now in ProductsController Index ##############")
     res.render("products/index", {});
   },
 
+  New: (req, res) => {
+    console.log("############## Now in ProductsController New ##############")
+    res.render("products/new", {});
+  },
+
   Create: (req, res) => {
-    console.log("############## Now in ProductController Create ##############")
+    console.log("############## Now in ProductsController Create ##############")
     const product = new Product(req.body);
     product.save();
     res.status(201).redirect("/")
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ProductsController = require("../controllers/products");
 
 router.get("/index", ProductsController.Index);
+router.get("/new", ProductsController.New);
 router.post("/", ProductsController.Create);
 
 module.exports = router;
