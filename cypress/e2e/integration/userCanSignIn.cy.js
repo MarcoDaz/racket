1aa6c073b4a402a9e7aedeea351252fd39cf47ec
Adrian Fee <adrian_fee@outlook.com>
2022-10-07 15:01:46 +0100

    cypress tests for user sign up and sign in complete and passing

---
+++ cypress/e2e/integration/userCanSignIn.cy.js
@@ -0,0 +1,18 @@
+describe("Authentication", () => {
+  it("A user signs in and is redirected to /", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#signedUpemail").type("someone@example.com");
+    cy.get("#signedUppassword").type("password");
+    cy.get("#signedUpsubmit").click();
+
+    cy.url().should("include", "/");
+    cy.contains("h1", "Butter");
+  });
+});
