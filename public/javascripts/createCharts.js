6c66820f86e37be7431ca59e25ad80e26afbdc84
chimaamu <chimaamu@proton.me>
2022-10-07 17:33:51 +0100

    removed ejs in createCharts.js

--- public/javascripts/createCharts.js
+++ public/javascripts/createCharts.js
@@ -2,7 +2,7 @@ const createCharts = (products, htmlId) => {
   // create canvases
   const container = document.getElementById(htmlId);
   const html = products.map((product) => (`
-    <a href="products/<%= product._id %>">
+    <a href="products/${product._id}">
       <h1>${product.name}</h1>
     </a>
     <canvas id="${product._id}" width="300" height="300"></canvas>
