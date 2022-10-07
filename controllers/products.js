bbe8bac1db8da9ab5172d66a5d222cc0b89f032e
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-10-07 10:43:04 +0100

    product search api created

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,26 @@
 const Product = require("../models/product");
 
 const ProductsController = {
-  Index: (req, res) => {
-    console.log("############## Now in ProductController New ##############")
-    res.render("products/index", {});
+  Search: async (req, res) => {
+    console.log("###### Now in ./controllers/products SEARCH ######");
+    const searchTerm = req.query.searchTerm;
+
+    let results = null;
+    if (searchTerm) {
+      results = await Product.find({
+        name: new RegExp(searchTerm, "i")
+      }).exec();
+    }
+    
+
+    res.json(results || "No Search Term Given");
   },
 
   Create: (req, res) => {
-    console.log("############## Now in ProductController Create ##############")
+    console.log("###### Now in ./controllers/products Create ######");
     const product = new Product(req.body);
     product.save();
-    res.status(201).redirect("/")
+    res.status(201).redirect("/");
   }
 }
 
