61206a00e062a902a633ac79782469bbdf97707c
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-10-08 20:19:06 +0100

    express.urlencoded extended:fase

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(methodOverride("_method"));
-// app.use('/scripts', express.static(__dirname + 'public'));
 
 app.use(
   session({
