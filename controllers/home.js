76f9547c720c6f703f332ad541ac813f7a863a46
Douglas Barco <douglas@dobuilding.co.uk>
2022-10-10 17:27:05 +0100

    Night mode in progress + session log out + login buttons gone after new session

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,7 @@
 const HomeController = {
   Index: async (req, res) => {
     console.log("############## Now in HomeController Index ##############")
-    
-    res.render("home/index", { title: 'Home'});
+    res.render("home/index", { title: 'Home', user: req.session.user});
   }
 }
 
--- routes/sessions.js
+++ routes/sessions.js
@@ -5,5 +5,6 @@ const SessionsController = require("../controllers/sessions");
 
 router.get("/new", SessionsController.New);
 router.post("/", SessionsController.Create);
+router.get("/delete", SessionsController.Destroy);
 
 module.exports = router;
