efc5254384ba553fc5307f8b669c649a3579cc06
chimaamu <chimaamu@proton.me>
2022-10-12 16:58:01 +0100

    changes made to navigation, and added about page

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,14 @@
+const { restart } = require("nodemon");
+
 const HomeController = {
   Index: async (req, res) => {
     console.log("############## Now in HomeController Index ##############")
     res.render("home/index", { title: 'Home', user: req.session.user});
+  },
+
+  About: (req, res) => {
+    console.log("############## Now in HomeController Index ##############")
+    res.render("home/about", { title: 'About', user: req.session.user})
   }
 }
 
