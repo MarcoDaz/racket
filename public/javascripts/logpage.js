4fc2e2ed60491fe9e91327c3f752b174ff657db4
Douglas Barco <douglas@dobuilding.co.uk>
2022-10-13 11:19:08 +0100

    animation + hotfix form new user

--- public/javascripts/logpage.js
+++ public/javascripts/logpage.js
@@ -13,3 +13,14 @@ signInButton.addEventListener('click', () => {
 });
 }
 
+(document).ready(() => {
+	let typingElement = $('.typing');
+  
+	typingElement.on('click', (e) => {
+	  typingElement.removeClass('animate');
+	  setTimeout(() => typingElement.addClass('animate'), 1);
+	})
+  });
+
+
+
